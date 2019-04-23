3ebb0b246a5bd5b450346195c0d8df2c779ca31b
bahador <taheri.amirbahador@gmail.com>
2019-04-23 16:02:45 +0200

    changed template

--- experiment/inserter.py
+++ experiment/inserter.py
@@ -1,7 +1,7 @@
 import psycopg2
 import datetime
 def insert(table, column, value):
-    connection = psycopg2.connect(dbname = 'thezdb', user= 'postgres')
+    connection = psycopg2.connect(dbnam='thezdb', user= 'postgres', password='123456')
     mark = connection.cursor()
     statement = 'INSERT INTO ' + table + ' (' + column + ') VALUES (' + value + ')'
     mark.execute(statement)
