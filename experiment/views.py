d85555d080e849243fd0a49cf2f95108d90d6714
bahador <taheri.amirbahador@gmail.com>
2019-04-24 15:57:20 +0200

    changes to exp.html

--- experiment/views.py
+++ experiment/views.py
@@ -8,8 +8,18 @@ from . import inserter
 def experiment(request):
     exp_answers= Experiment.objects
     answer = request.POST.get('MyRadio')
-    #inserter('results', 'result_answerlr', answer)
     return render(request, 'experiment.html', {'experimental':exp_answers})
+
+    def insert(table, column, value):
+        connection = psycopg2.connect(dbnam='thezdb', user='postgres', password='123456')
+        mark = connection.cursor()
+        statement = 'INSERT INTO ' + table + ' (' + column + ') VALUES (' + value + ')'
+        mark.execute(statement)
+        connection.commit()
+        return
+    inserted_data= insert('results', 'resultanswerlr', answer)
+
+
 def question_2(request):
     answer = str(request.POST.get('MyRadio'))
     inserter('results', 'result_answerlr', answer)
