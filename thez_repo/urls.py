0edac885961e09a334065ca19bf16c0c15190741
bahador <taheri.amirbahador@gmail.com>
2019-04-28 15:42:52 +0200

    changed the name of the project

--- thez_repo/urls.py
+++ thez_repo/urls.py
@@ -1,4 +1,4 @@
-"""thez URL Configuration
+"""thez_repo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
