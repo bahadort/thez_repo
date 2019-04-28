5ff1e1ecc080b929c89892e4b4d699b504aaeb4e
bahador <taheri.amirbahador@gmail.com>
2019-04-28 16:57:51 +0200

    changed views

--- experiment/views.py
+++ experiment/views.py
@@ -8,35 +8,19 @@ import datetime
 def experiment(request):
     exp_answers= Experiment.objects
     answer = request.POST.get('MyRadio')
-    answer_int= int(answer)
-    connection= psycopg2.connect(database='thezdb',
-                                 user='postgres',
-                                 password='123456',
-                                 host = "104.248.253.111",
-                                 port = "5432")
-    mark = connection.cursor()
-    table = 'results'
-    column = 'result_answerlr'
-    value = str(answer_int)
-    statement = 'INSERT INTO ' + table + ' (' + column + ') VALUES (' + value + ')'
-    mark.execute(statement)
-    connection.commit()
-    mark.close()
-    connection.close()
-
-
-    return render(request, 'experiment.html', {'experimental': exp_answers})
+    return render(request, 'experiment.html')
+    
 
 
 def question_2(request):
     answer = str(request.POST.get('MyRadio'))
-    inserter('results', 'result_answerlr', answer)
+
     return render(request, 'question-2.html', {'answer': answer})
 def question_3(request):
     answer = str(request.POST.get('MyRadio'))
-    inserter('results', 'result_answerlr', answer)
+
     return render(request, 'question-3.html', {'answer': answer})
 def refresh(request):
     answer = str(request.POST.get('MyRadio'))
-    inserter('results', 'result_answerlr', answer)
+
     return render(request, 'refresh.html', {'answer':answer})
